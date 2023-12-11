c2b3311584997251a9bc615800b7b4f26ff8ccb3
Brenna Fisher <2.bfisher@gmail.com>
2023-12-11 15:28:41 -0700

    login stuff

--- index.js
+++ index.js
@@ -58,4 +58,35 @@ app.get('/adminlogin', (req, res) => {
     res.render(path.join(__dirname + '/views/login'));
 });
 
+
+//I'm editing this right now (Brenna)
+app.post('/userLogin', (req, res) => {
+    //query that searches the database for a matching record,
+    knex('user')
+        .where('password', req.body.pword)
+        .andWhere('email', req.body.useremail)
+        .select('password', 'email', 'first_name', 'last_name', 'is_admin')
+        .then((results) => {
+            if (results.length == 0) {
+                //user credentials invalid
+                res.status(401).json({ message: 'Invalid Credentials' });
+            } else {
+                let sFirstName = results[0].first_name;
+                let sLastName = results[0].last_name;
+                let isAdmin = results[0].is_admin;
+
+                res.render(path.join(__dirname + '/views/testing'), {
+                    first_name: sFirstName,
+                    last_name: sLastName,
+                    is_admin: isAdmin,
+                    login: 'true',
+                });
+            }
+        });
+});
+
+
+
+
+
 app.listen(port, () => console.log('I am listening'));
