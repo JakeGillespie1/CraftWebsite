e33459f5bc0ff39106a74f3e4e6594fd66beeb73
ltsubaki <lilian.tsubaki@gmail.com>
2023-12-12 21:17:05 -0700

    delete band

--- index.js
+++ index.js
@@ -67,12 +67,12 @@ app.get('/edit/:id', (req, res) => {
         });
 });
 
-app.post('/deleteBand/:id', (req, res) => {
+app.post('/delete/:id', (req, res) => {
     console.log("Gabe is gay", req.params.id)
     knex('review')
         .where('review_id', "=", req.params.id)
         .del()
-        .then((reviewData) => {
+        .then(() => {
             res.redirect('/');
         })
         .catch((err) => {
