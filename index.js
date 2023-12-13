d41b737fc402100ab8fbeb9e631643da7c8f9ef2
jakerg@byu.edu <jakerg@byu.edu>
2023-12-12 20:16:37 -0700

    logged in

--- index.js
+++ index.js
@@ -110,12 +110,13 @@ app.post('/userLogin', (req, res) => {
                 //user credentials invalid
                 res.render(path.join(__dirname + '/views/errorPage'));
             } else {
+                let iLoggedIn = 1
+
                 knex.select()
                 .from('product')
                 .then((results) => {
                 res.render(path.join(__dirname + '/views/index'), {
-                    reviewData: results,
-                    //login: 'true', Add later?
+                    reviewData: results, logged_in : iLoggedIn,
                 });
                 });
             }
